ce97d610ef19acad03fcba19f63536fd38fbdc96
Zbigniew Tomanek <zbigniew.tomanek@protonmail.com>
2022-03-24 21:01:06 +0100

    configured picom

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ return {
   color_scheme = "Dracula +",
   adjust_window_size_when_changing_font_size = false,
   font_size = 9.0,
-  hide_tab_bar_if_only_one_tab = true
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.7,
 }
 
